0fdc00726345cc62f87048e812f601805a11328c
Takegat4356 <ssrankin@umich.edu>
2014-07-18 16:27:20 -0400

    Commit 2
    2nd Commit - Didn't have time to do the programming exercises. Will do
    later at home

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -64,6 +64,8 @@ namespace Loops
             //1. Write code to print all the names of all the studnets in the collection above.
             // 2. Modify the Student class to provide a method to print each students name.
             // 3. Use the method from (2) to print the students' names.
+            Console.WriteLine(allStudents);
+            Console.ReadLine();
         }
     }
 }
