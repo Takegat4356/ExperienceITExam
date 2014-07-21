e2b97a20c04add9b6188e963877d14e6e76d8704
Bill Wagner <bill.w.wagner@outlook.com>
2014-07-21 12:25:33 -0400

    Add the dice roll problem

---
+++ DiceRoll/Controllers/DiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiceRoll.Controllers
+{
+    public class DiceController : Controller
+    {
+        // GET: Dice
+        public ActionResult Index()
+        {
+
+            var rolls = from x in Enumerable.Range(1, 6)
+                        from y in Enumerable.Range(1, 6)
+                        select new DiceRoll.Models.DiceRoll
+                        { DieOne = x, DieTwo = y };
+            return View(rolls);
+        }
+    }
+}
---
+++ DiceRoll/Models/DiceRoll.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiceRoll.Models
+{
+    public class DiceRoll
+    {
+        public int DieOne { get; set; }
+        public int DieTwo { get; set; }
+    }
+}
